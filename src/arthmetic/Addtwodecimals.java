d368f9aeddf43d6abd5a14005349421bc0787627
shiva1821 <vishalmudhiraj151@gmail.com>
2026-04-01 22:49:13 +0530

    added loops

---
+++ src/arthmetic/Addtwodecimals.java
@@ -0,0 +1,11 @@
+package arthmetic;
+
+public class Addtwodecimals {
+	public static void main(String[] args) {
+		double x = 3.4;
+		double y = 4.4;
+		double sum = x+y;
+		System.out.println("Sum:" + sum);
+	}
+
+}
---
+++ src/arthmetic/Multiplication.java
@@ -0,0 +1,11 @@
+package arthmetic;
+
+public class Multiplication {
+	public static void main(String[]args) {
+	int a = 10;
+	int b = 2;
+	int c = a*b;
+	System.out.println("Mult="+c);	
+	}
+
+}
